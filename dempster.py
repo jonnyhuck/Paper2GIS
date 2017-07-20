5186cc599ef3a066ad23174a86f31d3d7366cfc4
Jonny Huck <jonnyhuck@gmail.com>
2017-07-20 13:06:36 +0100

    dempster shaffer stuff working now, but better as a single bar

--- dempster.py
+++ dempster.py
@@ -41,59 +41,30 @@ for c in contours:
 boundingBoxes = [cv2.boundingRect(c) for c in questionCnts]
 (contours, boundingBoxes) = zip(*sorted(zip(questionCnts, boundingBoxes), key=lambda b:b[1][0], reverse=False))
 
-print len(contours)
+# confidence
+ds_mass = 0
+
+# loop over the sorted contours
+for c in contours:
+
+	# construct a mask that reveals only the current "bubble" for the question
+	mask = np.zeros(thresh.shape, dtype="uint8")
+	cv2.drawContours(mask, [c], -1, 255, -1)
+	before = cv2.countNonZero(mask)
+
+	# apply the mask to the thresholded image, then count the number of non-zero pixels in the bubble area
+	mask = cv2.bitwise_and(thresh, thresh, mask=mask)
+	after = cv2.countNonZero(mask)
+
+	# get a mass value
+	ds_mass += after / before
+	
+print "I can see", len(contours), "bubbles, with a mass of", ds_mass
 
 # switch back to BGR and draw contours
 crop_img = cv2.cvtColor(crop_img,cv2.COLOR_GRAY2BGR)
 cv2.drawContours(crop_img, questionCnts, -1, (0,255,0), 2)
 
 # show the result
-cv2.imshow("cropped", crop_img)
-cv2.waitKey(0)
-
-
-'''
-HERE
-
-
-# loop over the sorted contours
-for (j, c) in enumerate(cnts):
-	# construct a mask that reveals only the current "bubble" for the question
-	mask = np.zeros(thresh.shape, dtype="uint8")
-	cv2.drawContours(mask, [c], -1, 255, -1)
-
-	# apply the mask to the thresholded image, then
-	# count the number of non-zero pixels in the
-	# bubble area
-	mask = cv2.bitwise_and(thresh, thresh, mask=mask)
-	total = cv2.countNonZero(mask)
-
-	# if the current total has a larger number of total
-	# non-zero pixels, then we are examining the currently
-	# bubbled-in answer
-	if bubbled is None or total > bubbled[0]:
-		bubbled = (total, j)
-		
-# initialize the contour color and the index of the
-# *correct* answer
-color = (0, 0, 255)
-k = ANSWER_KEY[q]
-
-# check to see if the bubbled answer is correct
-if k == bubbled[1]:
-	color = (0, 255, 0)
-	correct += 1
-
-# draw the outline of the correct answer on the test
-cv2.drawContours(paper, [cnts[k]], -1, color, 3)
-	
-# grab the test taker
-score = (correct / 5.0) * 100
-print("[INFO] score: {:.2f}%".format(score))
-cv2.putText(paper, "{:.2f}%".format(score), (10, 30),
-	cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
-cv2.imshow("Original", image)
-cv2.imshow("Exam", paper)
-cv2.waitKey(0)
-
-'''
+# cv2.imshow("cropped", crop_img)
+# cv2.waitKey(0)
